956682a333fe0b4b73b38b92b8b97a4a031204e4
levinweinstein <lweinstein@gomerits.com>
2019-09-16 13:02:03 -0700

    templateStrings with forEach and reduce

--- count-to-6/taggedTemplateStrings.js
+++ count-to-6/taggedTemplateStrings.js
@@ -13,7 +13,6 @@ console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
 function html(splitString, name, phrase) {
     let newItems = ["", ""];
-    let output = "";
 
     [name, phrase].forEach((item, index) => {
         item.split('').forEach((char) => {
@@ -22,10 +21,11 @@ function html(splitString, name, phrase) {
         });
     });
 
-    splitString.forEach((item, index) =>{
-        output += item;
-        if (index != splitString.length - 1)
-            output += newItems[index];
+    let output= splitString.reduce((accumulator, next, index) =>{
+        if (index < splitString.length)
+            accumulator += newItems[index - 1];
+        accumulator += next;
+        return accumulator;
     })
     return output;
 }
