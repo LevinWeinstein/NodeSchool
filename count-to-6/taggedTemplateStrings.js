9334befd9ba7ca3e6d200c693151cdf319da02f4
levinweinstein <lweinstein@gomerits.com>
2019-09-16 13:11:50 -0700

    Added dynamic argument count to taggedTemplateStrings solution

--- count-to-6/taggedTemplateStrings.js
+++ count-to-6/taggedTemplateStrings.js
@@ -4,23 +4,23 @@ var equivalencies = {
     "<" : "&lt;",
     ">" : "&gt;",
     "&" : "&amp;"
-};
+}
 
 console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
 
 
 
-function html(splitString, name, phrase) {
-    let newItems = ["", ""];
+function html(splitString, ...substitutions) {
+    let newItems = Array(substitutions.length).fill("");
 
-    [name, phrase].forEach((item, index) => {
+    substitutions.forEach((item, index) => {
         item.split('').forEach((char) => {
             newItems[index] += equivalencies[char] || char;
         });
     });
 
-    let output = splitString.reduce((accumulator, next, index) => {
+    let output= splitString.reduce((accumulator, next, index) =>{
         if (index < splitString.length)
             accumulator += newItems[index - 1];
         accumulator += next;
